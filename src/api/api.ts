82781b221c5d4b77f9510d4e244f618e4e980dae
TylerEisenhauer <tylereisenhauer7372@gmail.com>
2025-03-25 19:56:10 -0500

    Fix errors

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,70 +1,70 @@
-import axios, {AxiosInstance} from 'axios'
-import {Zone} from '../types/zone'
-import {DnsEntry} from '../types/dns-entry'
+import axios, { AxiosInstance } from 'axios'
+import { Zone } from '../types/zone.js'
+import { DnsEntry } from '../types/dns-entry.js'
 
 let client: AxiosInstance
 
 export function initializeApiClient() {
-    client = axios.create({
-        baseURL: 'https://api.cloudflare.com/client/v4/',
-        headers: {
-            Authorization: `Bearer ${process.env.CLOUDFLARE_API_KEY}`
-        }
-    })
+  client = axios.create({
+    baseURL: 'https://api.cloudflare.com/client/v4/',
+    headers: {
+      Authorization: `Bearer ${process.env.CLOUDFLARE_API_KEY}`
+    }
+  })
 }
 
 async function getIpAddress(): Promise<string> {
-    try {
-        const {data} = await axios.get('https://ifconfig.io/ip')
-        return data.replace('\n', '')
-    } catch (e) {
-        console.log('Error getting ip address')
-        throw e
-    }
+  try {
+    const { data } = await axios.get('https://ifconfig.io/ip')
+    return data.replace('\n', '')
+  } catch (e) {
+    console.log('Error getting ip address')
+    throw e
+  }
 }
 
 async function getZones(): Promise<Zone[]> {
-    try {
-        const {data} = await client.get('zones')
-        return data.result
-    } catch (e) {
-        console.log('Error getting zones')
-        throw e
-    }
+  try {
+    const { data } = await client.get('zones')
+    return data.result
+  } catch (e) {
+    console.log('Error getting zones')
+    throw e
+  }
 }
 
 async function getDnsEntry(zoneId: string): Promise<DnsEntry[]> {
-    try {
-        const {data} = await client.get(`zones/${zoneId}/dns_records`, {
-            params: {
-                type: 'A'
-            }
-        })
-        return data.result
-    } catch (e) {
-        console.log('Error getting dns entries')
-        throw e
-    }
+  try {
+    const { data } = await client.get(`zones/${zoneId}/dns_records`, {
+      params: {
+        type: 'A'
+      }
+    })
+    return data.result
+  } catch (e) {
+    console.log('Error getting dns entries')
+    throw e
+  }
 }
 
 async function updateDnsEntry(entry: DnsEntry) {
-    try {
-        const {data} = await client.put(`zones/${entry.zone_id}/dns_records/${entry.id}`, {
-            type: entry.type,
-            name: entry.name,
-            content: entry.content,
-            ttl: entry.ttl
-        })
-        return data.result
-    } catch (e) {
-        console.log('Error updating dns entries')
-        throw e
-    }
+  try {
+    const { data } = await client.put(`zones/${entry.zone_id}/dns_records/${entry.id}`, {
+      type: entry.type,
+      name: entry.name,
+      content: entry.content,
+      ttl: entry.ttl
+    })
+    return data.result
+  } catch (e) {
+    console.log('Error updating dns entries')
+    throw e
+  }
 }
 
 export default {
-    getZones,
-    getDnsEntry,
-    updateDnsEntry,
-    getIpAddress
+  getZones,
+  getDnsEntry,
+  updateDnsEntry,
+  getIpAddress
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import api, {initializeApiClient} from './api/api'
-import {Zone} from './types/zone'
-import {DnsEntry} from './types/dns-entry'
-import {config} from 'dotenv'
+import api, { initializeApiClient } from './api/api.js'
+import { Zone } from './types/zone.js'
+import { DnsEntry } from './types/dns-entry.js'
+import { config } from 'dotenv'
 
 config()
 initializeApiClient()
@@ -9,33 +9,35 @@ initializeApiClient()
 const minutes = 1
 const interval = minutes * 60 * 1000
 
-let ipAddressCache: string
+let ipAddressCache: string = await api.getIpAddress()
+await updateIpAddress(ipAddressCache)
 
 setInterval(async () => {
-    try {
-        const ipAddress = await api.getIpAddress()
+  try {
+    const ipAddress = await api.getIpAddress()
 
-        if (ipAddress !== ipAddressCache) {
-            await updateIpAddress(ipAddress)
+    if (ipAddress !== ipAddressCache) {
+      await updateIpAddress(ipAddress)
 
-            ipAddressCache = ipAddress
-        }
-    } catch (e) {
-        console.log(e.message)
+      ipAddressCache = ipAddress
     }
+  } catch (e) {
+    console.log(e.message)
+  }
 }, interval)
 
 async function updateIpAddress(ipAddress: string) {
-    const zones: Zone[] = await api.getZones()
+  const zones: Zone[] = await api.getZones()
 
-    for (let zone of zones) {
-        const dnsEntries: DnsEntry[] = await api.getDnsEntry(zone.id)
+  for (let zone of zones) {
+    const dnsEntries: DnsEntry[] = await api.getDnsEntry(zone.id)
 
-        for (let entry of dnsEntries) {
-            entry.content = ipAddress
+    for (let entry of dnsEntries) {
+      entry.zone_id = zone.id
+      entry.content = ipAddress
 
-            const newEntry = await api.updateDnsEntry(entry)
-            console.log(`IP Address updated for ${newEntry.name}\n${JSON.stringify(newEntry, null, 2)}`)
-        }
+      const newEntry = await api.updateDnsEntry(entry)
+      console.log(`IP Address updated for ${newEntry.name}\n${JSON.stringify(newEntry, null, 2)}`)
     }
+  }
 }
